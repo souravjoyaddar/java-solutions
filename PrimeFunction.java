1b15b24da71353190c1ec98d4b538591847789e0
souravjoyaddar <souravjoadder@gmail.com>
2025-04-06 22:48:21 +0600

    optimize solution added

--- PrimeFunction.java
+++ PrimeFunction.java
@@ -1,20 +1,33 @@
+//prime number
 public class PrimeFunction {
     //function to calculate prime number
+    // public static boolean checkPrime(int n) {
+    //     //corner case for n <= 1
+    //     if (n <= 1) { 
+    //         return false;
+    //     } 
+    //     for ( int i = 2; i <= n-1; i++) {
+    //         if (n % i == 0) {
+    //             return false;
+    //         }    
+    //     }
+    //     return true;
+    // }
+
+    //optimize approach to calculate prime number
     public static boolean checkPrime(int n) {
-        //corner case for n <= 1
-        if (n <= 1) { 
-            return false;
-        } 
-        for ( int i = 2; i <= n-1; i++) {
+        if (n <= 1) {
+           return false; 
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 return false;
-            }    
+            }
         }
         return true;
     }
     
     public static void main(String[] args) {                                
-        System.out.println(checkPrime(1));
+        System.out.println(checkPrime(2));
     } 
-
-}
+} 
