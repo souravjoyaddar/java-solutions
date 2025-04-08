705102cc5a529bb65a711c99c60741baaba05173
souravjoyaddar <souravjoadder@gmail.com>
2025-04-08 23:20:36 +0600

    evenOdd solution added

--- AvgCalculator.java
+++ AvgCalculator.java
@@ -5,7 +5,6 @@ public class AvgCalculator {
         return (num1 + num2 + num3) / 3;
     
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter 3 number to calculate the average: ");
@@ -13,7 +12,7 @@ public class AvgCalculator {
         int num2 = sc.nextInt();
         int num3 = sc.nextInt();
         int result = getAvg(num1, num2, num3);
-        sc.close();
         System.out.println("The average of 3 numbers is = " + result);
+        sc.close();
     }
 }
