ec5954ae562e38c6839c86fb1aa03ffcbebb5a8e
souravjoyaddar <souravjoadder@gmail.com>
2025-04-13 22:27:53 +0600

    zero one traingle pattern

--- Patterns.java
+++ Patterns.java
@@ -64,14 +64,27 @@ public class Patterns {
             System.out.println();
         }
     }
-
+    public static void zeroOneTraingle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ( (i + j) % 2 == 0) {
+                    System.out.print(1 + " ");
+                } else {
+                    System.out.print(0 + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    //driver code
     public static void main(String[] args) {
         // hollowRectngle(3, 4);
         // invertedRoatedHalfPyramiid(5);
         // invertedPyramidWithNumbers(6);
         // pyramidWithNumbers(5);
         // invrPyraWithNum2(5);
-        floydsTrangle(5);
+        // floydsTrangle(5);
+        zeroOneTraingle(5);
 
     }
 }
