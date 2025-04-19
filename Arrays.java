85a19c02b89981ca71aa5e885cf213ced751833f
souravjoyaddar <souravjoadder@gmail.com>
2025-04-19 22:52:47 +0600

    initialized an array

---
+++ Arrays.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Arrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int marks[] = new int[50];
+
+        int phy = sc.nextInt();
+        int chem = sc.nextInt();
+        int math = sc.nextInt();
+
+        
+
+    }
+    
+}
