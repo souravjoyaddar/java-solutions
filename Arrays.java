a679f393bc7ba465cf57bfbc7c8b632d75c976f1
souravjoyaddar <souravjoadder@gmail.com>
2025-04-22 13:54:01 +0600

    max and min number of an array added

--- Arrays.java
+++ Arrays.java
@@ -1,24 +1,25 @@
-import java.util.*;
 public class Arrays {
-    //arrays in java are passed by refenance, which menans the modifications inside function will effect the main function
-    public static void update(int marks[], int notChanged) {
-        notChanged = 10;
-        for (int i=0; i<marks.length; i++) {
-            marks[i] += 1;
-        }
+    public static int getMax(int numbers[]) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
 
-    }
-    public static void main(String[] args) {
-        // Scanner sc = new Scanner(System.in);
-        int marks[] = {88, 92, 95};
-        int notChanged = 5;
-        update(marks, notChanged);
-        System.out.println("Unchanged valu: " +notChanged);
-        //indecis with incrimented value by 1 using update() function
-        for (int i=0; i<marks.length; i++) {
-            System.out.print(marks[i]+" ");
+        for (int i=0; i<numbers.length; i++) {
+            if (max < numbers[i]) {
+                max = numbers[i];    
+            }
+            if (min > numbers[i]) {
+                min = numbers[i];
+            }
         }
-        System.out.println();
+        System.out.println("The smallest number of the array is: " + min);
+        return max;
+    }
+    
+    public static void main(String[] args) {
+        int numbers[] = {4, 6, 8, 10, 18, 12, 14, 1, 16};
+
+        System.out.println("The largest num of array is: " + getMax(numbers));
+        
 
     }
     
