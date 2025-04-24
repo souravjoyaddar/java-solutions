dc2e1af9bc90cdc5205dd5ef545ab8f48f4e1a1f
souravjoyaddar <souravjoadder@gmail.com>
2025-04-24 23:36:50 +0600

    BinarySearch modified

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,8 +1,12 @@
 public class BinarySearch {
     public static int binarySearch(int numbers[], int target) {
-        int start = 0, end = numbers.length-1;
+        int start = 0, end = numbers.length -1;
         while (start <= end) {
+            //calculating the mid
+            // int mid = start + ((end - start) / 2); //for bigger int
             int mid = (start + end) / 2;
+
+            //comperision
             if (numbers[mid] == target) {
                 return mid;
             }
@@ -16,13 +20,13 @@ public class BinarySearch {
     }
     //driver code
     public static void main(String[] args) {
-        int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16};
-        int target = 20;
+        int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 9;
         int index = binarySearch(numbers, target);
         if (index == -1) {
-            System.out.println("Target NOT Found.");
+            System.out.println("Index NOT found.");
         } else {
-            System.out.println("Target is found at index: " + index);
+            System.out.println(numbers[index] + " is found at index " + index);
         }
     }
 }
