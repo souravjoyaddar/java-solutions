17181d101c572095d7af65257d307f567564fdf1
souravjoyaddar <souravjoadder@gmail.com>
2025-04-26 11:15:46 +0600

    array pairs code updated

--- ArrayPairs.java
+++ ArrayPairs.java
@@ -1,12 +1,15 @@
 public class ArrayPairs {
     public static void printPairs(int[] numbers) {
+        int tp = 0;
         for (int i=0; i<numbers.length; i++) {
             int current = numbers[i]; // 2, 4, 6, 8, 10
             for (int j=i+1; j<numbers.length; j++) {
                 System.out.print("(" + current + "," + numbers[j]+ ") " );
+                tp++;
             }
             System.out.println();
         }
+        System.out.println("Total pair is = " + tp);
     }
 
     //driver code
